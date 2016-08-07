c2ccd5a741c74492dfc972eaf72b7a6d1ca76a85
Ahmed Yakout <iyakout@hotmail.com>
2016-08-07 15:09:02 +0200

    testing source control.

--- I Was Here/ViewController.swift
+++ I Was Here/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("testing git")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
