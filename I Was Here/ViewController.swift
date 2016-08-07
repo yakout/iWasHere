4719c7d6d9fc6e758ded9749d0ad652fe46081e4
Ahmed Yakout <iyakout@hotmail.com>
2016-08-07 15:02:09 +0200

    Initial Commit

---
+++ I Was Here/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Was Here
+//
+//  Created by Ahmed Yakout on 8/7/16.
+//  Copyright © 2016 iYakout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
