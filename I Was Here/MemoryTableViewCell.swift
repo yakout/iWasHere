df67e83ac0a1f344e643649aa20bae41761d6e94
Ahmed Yakout <iyakout@hotmail.com>
2016-08-08 17:57:14 +0200

    storyboard and overview Design (v1)

---
+++ I Was Here/MemoryTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MemoryTableViewCell.swift
+//  iWasHere
+//
+//  Created by Ahmed Yakout on 8/8/16.
+//  Copyright © 2016 iYakout. All rights reserved.
+//
+
+import UIKit
+
+class MemoryTableViewCell: UITableViewCell {
+    
+    
+    // here you drag the custom cell items
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
